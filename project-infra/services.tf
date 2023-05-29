523ed07bfaff35c1ec7c3133bdff2adb1f02d558
John Nguyen <nguy4130@umn.edu>
2023-05-28 23:05:55 -0400

    tf fmt

--- project-infra/services.tf
+++ project-infra/services.tf
@@ -1,9 +1,9 @@
 resource "google_project_service" "cloud-functions" {
-    project = local.project-id
-    service = "cloudfunctions.googleapis.com"
+  project = local.project-id
+  service = "cloudfunctions.googleapis.com"
 }
 
 resource "google_project_service" "cloud-build" {
-    project = local.project-id
-    service = "cloudbuild.googleapis.com"
+  project = local.project-id
+  service = "cloudbuild.googleapis.com"
 }
