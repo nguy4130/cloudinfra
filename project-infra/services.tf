91da56a3f7702361490deea2f3424975b1bbe442
John Nguyen <nguy4130@umn.edu>
2023-05-28 23:04:31 -0400

    add cf

---
+++ project-infra/services.tf
@@ -0,0 +1,9 @@
+resource "google_project_service" "cloud-functions" {
+    project = local.project-id
+    service = "cloudfunctions.googleapis.com"
+}
+
+resource "google_project_service" "cloud-build" {
+    project = local.project-id
+    service = "cloudbuild.googleapis.com"
+}
