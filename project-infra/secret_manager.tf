b4c7032f72c0861994af29e7dc66862912c1b005
John Nguyen <nguy4130@umn.edu>
2025-01-07 12:18:08 +0700

    add send grid and api key

---
+++ project-infra/secret_manager.tf
@@ -0,0 +1,11 @@
+resource "google_secret_manager_secret" "my-secret" {
+  provider = google-beta
+
+  secret_id = "sendgrid-api-key"
+
+  replication {
+    automatic = true
+  }
+
+  depends_on = [google_project_service.secretmanager]
+}
